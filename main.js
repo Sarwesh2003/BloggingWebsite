f030f5eef1c4e6357540c42a81f740dcb78f40ad
Sarwesh2003 <sarweshkhairnar@gmail.com>
2022-02-03 15:15:20 +0530

    ADS

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.con');
-
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
@@ -23,3 +22,26 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+function Next(){
+	switch(document.title){
+		case "ADS-1":
+			location.href="ads2.html";
+			break;
+		case "ADS-2":
+			location.href="ads3.html";
+		case "ADS-3":
+			alert("On Last Page");
+	}
+}
+function Previous(){
+	switch(document.title){
+		case "ADS-1":
+			alert("On First Page");
+			break;
+		case "ADS-2":
+			location.href="ads1.html";
+		case "ADS-3":
+			location.href="ads2.html";
+	}
+}
